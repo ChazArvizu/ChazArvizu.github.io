6cf6d47b8b00db5357405f6e272a9e0ba9d89ad6
ChazArvizu <cryan018@gmail.com>
2024-07-08 13:16:07 -0400

    new resume

--- docs/.vuepress/.temp/internal/siteData.js
+++
@@ -1,1 +0,0 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"en-US\",\"title\":\"Chaz Arvizu - Portfolio\",\"description\":\"Portfolio showcasing Chaz Arvizu's work\",\"head\":[],\"locales\":{}}")
